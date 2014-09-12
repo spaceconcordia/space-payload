a3cf30b9a65750803722226c7e850bdebf0b9c05
A-Hubbell <austin.hubbell@gmail.com>
2014-09-12 11:49:38 -0400

    Added fake data files for payload testing.
    Added three text files with fake data generated using matlab. There is
    a test info file that shows the peak values and their locations for
    each data set (expected results). Payload program has a problem
    recognizing “atof()”, does not compile

--- src/space-payload.cpp
+++ src/space-payload.cpp
@@ -9,7 +9,7 @@ Note:
  -The simulation uses one peak per time iteration, so peak frequency is
   determined by the number of measurements/transmissions
 
- Pseudo-Code:
+Pseudo-Code:
    -Find the maximum value of a certain group of digital signals
    -Record the frequency of events (frequency of the digital signals)
    -Log input signal (from amplifier going to geiger counter)
@@ -22,13 +22,14 @@ Questions:
   timestamp attached to each data element or should the program request
   the current time from the clock and record it? Timestamp
  -How to get the current time from onboard RTC?
+ -What to do if there are two max peaks of the same value in one transmission?
 
 TODO:
    -Figure out how to process binary data (how to consider a bitstring as
     a number and not an array of characters), easy if conversion is left
     out of simulation
    -Implement logging using shakespeare
-   -Add timestamp to each measurement
+   -Add timestamp to each measurement using millisecond precision
  
 */
 
@@ -38,6 +39,8 @@ TODO:
 #include <math.h>
 #include <array>
 #include <fstream>
+#include <string>
+#include <cstdlib>
 
 using namespace std;
 
@@ -45,16 +48,37 @@ using namespace std;
 
 int main(int argc, const char * argv[])
 {
-    
-    ofstream rawPeaksFile;
-    rawPeaksFile.open("rawPeaksData.txt");
-    ofstream maxPeaksFile;
-    maxPeaksFile.open("maxPeaksData.txt");
-    
+ 
     //Number of data elements sent in each transmission, number of transmissions
     //NOTE: These values determine the "length" of the simulation
     const int NUM_MEASUREMENTS = 1000;
     const int NUM_TRANSMISSIONS = 5;
+    
+    //Creates an array with each column corresponding to one transmission and
+    //each row within a colum being one measurement in that transmission
+    double analogPeaks[NUM_MEASUREMENTS][NUM_TRANSMISSIONS] = {0};
+    
+    //TODO: Get peaks data using I2C instead of text file
+    string line;
+    ifstream rawPeaksFile("rawPeaksData.txt");
+    if(rawPeaksFile.is_open()) {
+        //TODO: Ensure parsing is correct for the data coming in
+        int row = 0;
+        int column = 0;
+        while(getline(rawPeaksFile, line)) {
+            if(row < (NUM_MEASUREMENTS-1)) {
+                analogPeaks[row][column] = atof(line);
+                row += 1;
+                continue;
+            }
+            if(row == (NUM_MEASUREMENTS-1)) {
+                analogPeaks[row][column] = atof(line);
+                row = 0;
+                column += 1;
+            }
+        }
+        rawPeaksFile.close();
+    }
     
     //Fill time array with values (each column corresponds to one "transmission")
     int time[NUM_MEASUREMENTS][NUM_TRANSMISSIONS] = {0};
@@ -64,37 +88,6 @@ int main(int argc, const char * argv[])
         for (int j=0; j<NUM_MEASUREMENTS; j++) {
             time[j][i] = count;
             count += 1;
-        }
-    }
-    
-    //This step is only necessary for the simulation
-    //Creates an array with each column corresponding to one transmission and
-    //each row within a colum being one measurement in that transmission
-    double analogPeaks[NUM_MEASUREMENTS][NUM_TRANSMISSIONS] = {0};
-    
-    //TODO: Set appropriate max peak voltage
-    const int MAX_PEAK_VOLTAGE = 250;
-    const int GAIN = 100;
-    const int GAIN2 = 60;
-    
-    //TODO: move to Gtest
-    //Generate random peak data, most data should be below a certain threshold
-    for (int i=0; i<NUM_TRANSMISSIONS; i++) {
-        for (int j=0; j<NUM_MEASUREMENTS; j++) {
-            double someNum = (double)rand()/RAND_MAX;
-            analogPeaks[j][i] = ceil(someNum*MAX_PEAK_VOLTAGE);
-        }
-    
-        for (int j=0; j<NUM_MEASUREMENTS; j++) {
-            double someNum = (double)rand()/RAND_MAX;
-            analogPeaks[(int)ceil(someNum*NUM_MEASUREMENTS)][i] = ceil(someNum*GAIN);
-            //Randomize peak data even more
-            if(j%2 == 0) {
-                double someNum = (double)rand()/RAND_MAX;
-                analogPeaks[j][i] = ceil(someNum*GAIN2);
-            }
-            //Debugging only, write raw peaks data to file
-            rawPeaksFile << to_string(analogPeaks[j][i]) << endl;
         }
     }
     
@@ -111,63 +104,31 @@ int main(int argc, const char * argv[])
         }
         
         double peakData[NUM_MEASUREMENTS] = {0};
-        
         for(int j=0; j<NUM_MEASUREMENTS; j++) {
             peakData[j] = analogPeaks[j][i];
         }
     
-        
         //Compare all elements of peakData to find the max
         //After this is run, the max will be stored in "peakVal"
         double maxPeakVal = peakData[0];
-        int peakValTime = timeData[0];
+        int maxPeakValTime = timeData[0];
         for (int j=1; j<NUM_MEASUREMENTS; j++) {
             if (peakData[j] > maxPeakVal) {
                 maxPeakVal = peakData[j];
-                peakValTime = timeData[j];
+                maxPeakValTime = timeData[j];
             }
         }
     
         //Log the peak value and time to the permanent storage array
         //TODO: shakespeare logging here
-        maxPeaks[i][0] = peakValTime;
+        maxPeaks[i][0] = maxPeakValTime;
         maxPeaks[i][1] = maxPeakVal;
     
-        //Debugging only: log max peaks and their times
-        maxPeaksFile << to_string(peakValTime) + ", " + to_string(maxPeakVal) <<endl;
-    
+        cout << maxPeakVal << endl;
+        cout << maxPeakValTime << endl;
     }
     
-    
-    /* TODO: Plotting (look into Gnuplot)
-    
-    figure;
-    
-    subplot(2,1,1);
-    plot(time, analogPeaks, '.');
-    title('Randomized Peak Data', 'FontSize', 18);
-    axis([0 1 0 (MAX_PEAK_VOLTAGE+10)]);
-    xlabel('Time (s)');
-    ylabel('Amplitude (V)');
-    
-    subplot(2,1,2);
-    plot(maxPeaks(:,1), maxPeaks(:,2), '.');
-    title('Maximum Peaks from Each Transmission', 'FontSize', 18);
-    axis([0 1 0 (MAX_PEAK_VOLTAGE*1.75)]);
-    xlabel('Time (s)');
-    ylabel('Amplitude (V)');
-    
-    */
-    
-    rawPeaksFile.close();
-    maxPeaksFile.close();
-    
-  
-    //------------------------------Payload Software Tests--------------------------------------
-    
-    
-    
-    
+    //TODO: Log "analogPeaks" and "maxPeaks" using shakespeare
     
     cout << "Simulation Terminated Successfully!\n";
     return 0;
