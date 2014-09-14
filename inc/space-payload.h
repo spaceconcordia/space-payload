950659b55f2055ac6da46c6c478f5b6b40c1952a
A-Hubbell <austin@Austins-iMac.local>
2014-09-14 19:32:19 -0400

    Major refactoring and updating logic
    - Measure elapsed time between geiger activation and event detection
    - 2ms dead time after event detection
    - Compartmentalize code and move functions to header file
    - Code currently compiles, but runtime seg fault

--- inc/space-payload.h
+++ inc/space-payload.h
@@ -3,15 +3,15 @@
  *
  *       Filename:  space-payload.h
  *
- *    Description:  
+ *    Description:
  *
  *        Version:  1.0
  *        Created:  07/10/2014 07:04:39 PM
  *       Revision:  none
  *       Compiler:  gcc
  *
- *         Author:  YOUR NAME (), 
- *   Organization:  
+ *         Author:  YOUR NAME (),
+ *   Organization:
  *
  * =====================================================================================
  */
@@ -19,7 +19,74 @@
 #ifndef SPACE_PAYLOAD_H
 #define SPACE_PAYLOAD_H
 
-// TODO prototype all functions here
+using namespace std;
+
+//Prototype all functions here
+
+//Function for reading in data from a text file
+//Input file is assumed to be in the same directory as this program
+unsigned long long *readBinaryData (string fileName, long numLines) {
+  unsigned long long data[numLines];
+  string line;
+  ifstream dataFile(fileName);
+  if(dataFile.is_open()) {
+    unsigned long count = 0;
+    while(getline(dataFile, line)) {
+      data[count] = atoi(line.c_str());
+    }
+    dataFile.close();
+  }
+  else {
+    //TODO: Else condition if file doesn't open (if data is not available?)
+  }
+  unsigned long long *dataPointer = data;
+  return dataPointer;
+}
+
+//Reshape a one-dimensional array into a two-dimensional array (non-jagged assumed)
+unsigned long long **reshape (unsigned long long *oneDimData, unsigned long numRows, unsigned long numColumns) {
+  unsigned long row = 0;
+  unsigned long column = 0;
+  unsigned long long **twoDimData;
+
+  for (int i=0; i<sizeof(oneDimData); i++) {
+    if(row < (numRows)) {
+      twoDimData[row][column] = oneDimData[i];
+      row += 1;
+      continue;
+    }
+    if(row == (numRows-1)) {
+      twoDimData[row][column] = oneDimData[i];
+      row = 0;
+      column += 1;
+    }
+  }
+  unsigned long long **twoDimDataPointer = twoDimData;
+  return twoDimDataPointer;
+}
+
+/*
+
+//Finds the maximum value in dataArray, and the corresponding time element
+//The elements in "timeArray" must correspond with the values in "dataArray"
+float findMaxTimeAndVal[2] (float dataArray[], float timeArray) {
+  float maxVal = dataArray[0];
+  int maxValTime = timeArray[0];
+
+  for(int i=1; j<dataArray.length; i++) {
+    if(peakData[i] > maxVal) {
+      maxVal = peakData[i];
+      maxValTime = timeArray[j];
+    }
+  }
+
+  float returnArray[] = {maxValTime, maxVal};
+
+  return returnArray;
+}
+
+*/
+
+
 
 #endif /* !SPACE_PAYLOAD_H */
-
--- src/space-payload.cpp
+++ src/space-payload.cpp
@@ -4,32 +4,32 @@ Consat-1 Payload Simulation Version 2
 Author: Austin Hubbell (August 2014)
 
 Note:
- -One "measurement" is a single peak value
- -One "transmission" is a group of peak values sent in between dead times
- -The simulation uses one peak per time iteration, so peak frequency is
-  determined by the number of measurements/transmissions
+-One "measurement" is a single peak value
+-One "transmission" is a group of peak values sent in between dead times
+-The simulation uses one peak per time iteration, so peak frequency is
+determined by the number of measurements/transmissions
 
 Pseudo-Code:
-   -Find the maximum value of a certain group of digital signals
-   -Record the frequency of events (frequency of the digital signals)
-   -Log input signal (from amplifier going to geiger counter)
-   -Logging should occur after each transmission (group of measurements)
+-Find the maximum value of a certain group of digital signals
+-Record the frequency of events (frequency of the digital signals)
+-Log input signal (from amplifier going to geiger counter)
+-Logging should occur after each transmission (group of measurements)
 
 Questions:
- -The values being read in from Gieger counter, are they 8-byte floats?
- -How many measurements will be sent inbetween deadtimes (NUM_MEASUREMENTS)?
- -How should the rate of radiation events be determined, is there a
-  timestamp attached to each data element or should the program request
-  the current time from the clock and record it? Timestamp
- -How to get the current time from onboard RTC?
- -What to do if there are two max peaks of the same value in one transmission?
+-The values being read in from Gieger counter, are they 8-byte floats?
+-How many measurements will be sent inbetween deadtimes (NUM_MEASUREMENTS)?
+-How should the rate of radiation events be determined, is there a
+timestamp attached to each data element or should the program request
+the current time from the clock and record it? Timestamp
+-How to get the current time from onboard RTC?
+-What to do if there are two max peaks of the same value in one transmission?
 
 TODO:
-   -Figure out how to process binary data (how to consider a bitstring as
-    a number and not an array of characters), easy if conversion is left
-    out of simulation
-   -Implement logging using shakespeare
-   -Add timestamp to each measurement using millisecond precision
+-Figure out how to process binary data (how to consider a bitstring as
+a number and not an array of characters), easy if conversion is left
+out of simulation
+-Implement logging using shakespeare
+-Add timestamp to each measurement using millisecond precision
 stop 2ms
 
 maketime()
@@ -39,113 +39,64 @@ check if in anomoly
 
 
 #include <iostream>
-#include <stdio.h>
-#include <math.h>
 #include <fstream>
-#include <string>
-#include <cstdlib>
-
-#define LOG_PATH "/home/logs"
+#include "../inc/space-payload.h"
 
 using namespace std;
 
+#define LOG_PATH "/home/logs"
 #define PROCESS "payload"
-#define RAW_PEAKS_FILE_PATH "file path here"
+#define RAW_PEAKS_FILE_PATH "rawPeaksData1.txt"
 
 int main(int argc, const char * argv[])
 {
+  //Number of data elements sent in each transmission, total number of transmissions
+  const int NUM_MEASUREMENTS = 1000;
+  const int NUM_TRANSMISSIONS = 5;
 
-    //Number of data elements sent in each transmission, number of transmissions
-    //NOTE: These values determine the "length" of the simulation
-    const int NUM_MEASUREMENTS = 1000;
-    const int NUM_TRANSMISSIONS = 5;
 
-    //Creates an array with each column corresponding to one transmission and
-    //each row within a colum being one measurement in that transmission
-    float analogPeaks[NUM_MEASUREMENTS][NUM_TRANSMISSIONS] = {0};
+  //TODO: Use I2C to retrieve data and store in binaryPeakData array
+  //1-dimensional array storing peak magnitude values
+  unsigned long long *binaryPeaks;
+  binaryPeaks = readBinaryData(RAW_PEAKS_FILE_PATH, NUM_MEASUREMENTS*NUM_TRANSMISSIONS);
+  //2-dimensional array, each column is one "transmission"
+  unsigned long long **binaryPeakData = reshape(binaryPeaks, NUM_MEASUREMENTS, NUM_TRANSMISSIONS);
 
-    //TODO: Get peaks data using I2C instead of text file
-    //TODO: Consolodate into "readData" function
-    string line;
-    ifstream rawPeaksFile("rawPeaksData.txt");
-    if(rawPeaksFile.is_open()) {
-        //TODO: Ensure parsing is correct for the data coming in
-        int row = 0;
-        int column = 0;
-        while(getline(rawPeaksFile, line)) {
-            if(row < (NUM_MEASUREMENTS-1)) {
-                analogPeaks[row][column] = atof(line.c_str());
-                row += 1;
-                continue;
-            }
-            if(row == (NUM_MEASUREMENTS-1)) {
-                analogPeaks[row][column] = atof(line.c_str());
-                row = 0;
-                column += 1;
-            }
-        }
-        rawPeaksFile.close();
+  //Time between geiger counter activation and event detection
+  float eventTimes[NUM_MEASUREMENTS][NUM_TRANSMISSIONS] = {0};
+
+  //------------------Payload processing logic begins here----------------------
+  //Timing variables
+  clock_t start_time;
+  clock_t elapsed_time;
+
+
+  for (int i=0; i<(sizeof(binaryPeakData[0])/sizeof(long)); i++) {
+    unsigned long long tempBinaryPeakData[sizeof(binaryPeakData)/sizeof(binaryPeakData[0])];
+    float tempTimeData[sizeof(binaryPeakData)/sizeof(binaryPeakData[0])];
+    for(int j=0; j<(sizeof(binaryPeakData)/sizeof(binaryPeakData[0])); j++) {
+      //TODO: Activate geiger counter
+
+      start_time = clock();
+
+      //TODO: Check if an event has occurred
+
+      //Once event is detected, determine elapsed time for detection
+      elapsed_time = clock() - start_time;
+
+      //Begin 2ms dead time
+      start_time = clock();
+      while((clock() - start_time) < 2e-3*CLOCKS_PER_SEC) {
+        //Do logging in here so this time isn't wasted?
+      }
+
+      //Log peak magnitude and detection times to temporary variables
+      tempTimeData[j] = ((float)elapsed_time)/CLOCKS_PER_SEC;
+      tempBinaryPeakData[j] = binaryPeakData[j][i];
     }
+    //TODO: Log all data for this transmission using shakespeare
+  }
 
-    //Fill time array with values (each column corresponds to one "transmission")
-    int time[NUM_MEASUREMENTS][NUM_TRANSMISSIONS] = {0};
-    int count = 0;
-
-    for (int i=0; i<NUM_TRANSMISSIONS; i++) {
-        for (int j=0; j<NUM_MEASUREMENTS; j++) {
-            time[j][i] = count;
-            count += 1;
-        }
-    }
-
-    //Array to store the max value of each transmission
-    float maxPeaks[NUM_TRANSMISSIONS][2] = {0};
-
-    //For simplification purposes, the data is assumed to have already been
-    //converted from bitstrings to parsed decimal numbers
-    for (int i=0;i<NUM_TRANSMISSIONS; i++) {
-        //Create array of radiation data with associated timestamps for this
-        //specific transmission
-        int timeData[NUM_MEASUREMENTS] = {0};
-        for (int j=0; j<NUM_MEASUREMENTS; j++) {
-            timeData[j] = time[j][i];
-        }
-
-        float peakData[NUM_MEASUREMENTS] = {0};
-        for(int j=0; j<NUM_MEASUREMENTS; j++) {
-            peakData[j] = analogPeaks[j][i];
-        }
-
-        //Log the peak value and time to the permanent storage array
-        //TODO: shakespeare logging here
-        //TODO: consolodate into "logPeaks" function
-        maxPeaks[i][0] = findMaxValAndTime(peakData, timeData)[0];
-        maxPeaks[i][1] = findMaxValAndTime(peakData, timeData)[1];
-
-        cout << maxPeakVal << endl;
-        cout << maxPeakValTime << endl;
-    }
-
-    //TODO: Log "analogPeaks" and "maxPeaks" using shakespeare
-
-    cout << "Simulation Terminated Successfully!\n";
+    cout << "Space-Payload Terminated Successfully!\n";
     return 0;
-}
-
-//Finds the maximum value in dataArray, and the corresponding time element
-//The elements in "timeArray" must correspond with the values in "dataArray"
-float findMaxTimeAndVal[2] (float dataArray[], float timeArray) {
-    float maxVal = dataArray[0];
-    int maxValTime = timeArray[0];
-
-    for(int i=1; j<dataArray.length; i++) {
-        if(peakData[i] > maxVal) {
-          maxVal = peakData[i];
-          maxValTime = timeArray[j];
-        }
-    }
-
-    float returnArray[] = {maxValTime, maxVal};
-
-    return returnArray;
 }
